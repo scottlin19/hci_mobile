0e5acfb6fdc580e66600ed318f412a2124fc6637
ncomerci <ncomerci@itba.edu.ar>
2020-06-12 18:56:08 -0300

    Main structure settled

--- app/src/main/java/ar/edu/itba/hci/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/MainActivity.java
@@ -1,8 +1,25 @@
 package ar.edu.itba.hci;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavInflater;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import ar.edu.itba.hci.ui.home.HomeFragment;
+import ar.edu.itba.hci.ui.routines.RoutinesFragment;
+import ar.edu.itba.hci.ui.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpNavigation();
+    }
+
+    public void setUpNavigation(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
     }
 }
---
+++ app/src/main/java/ar/edu/itba/hci/ui/routines/RoutinesViewModel.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.hci.ui.routines;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RoutinesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public RoutinesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is routine fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
