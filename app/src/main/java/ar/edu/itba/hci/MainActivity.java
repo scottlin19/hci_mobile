87d1049b0fa8ed628238693cf2f2eebed1df2347
ncomerci <ncomerci@itba.edu.ar>
2020-06-12 21:42:54 -0300

    DarkMode switch

--- app/src/main/java/ar/edu/itba/hci/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/MainActivity.java
@@ -1,25 +1,12 @@
 package ar.edu.itba.hci;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.NavGraph;
-import androidx.navigation.NavInflater;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-
-import ar.edu.itba.hci.ui.home.HomeFragment;
-import ar.edu.itba.hci.ui.routines.RoutinesFragment;
-import ar.edu.itba.hci.ui.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/ar/edu/itba/hci/ui/settings/SettingsFragment.java
+++ app/src/main/java/ar/edu/itba/hci/ui/settings/SettingsFragment.java
@@ -4,32 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import ar.edu.itba.hci.MainActivity;
 import ar.edu.itba.hci.R;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
+    private Switch themeSwitch;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_settings);
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        themeSwitch = root.findViewById(R.id.theme_switch);
+        themeSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                if(themeSwitch.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
             }
         });
         return root;
     }
+
 }
